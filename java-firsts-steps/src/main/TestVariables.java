62cf0f2c352d86f74caed5cb9c38451dc2a2af53
kevin camacho <kevin97camacho@gmail.com>
2023-01-19 10:07:10 -0400

    Test variables

--- java-firsts-steps/src/main/TestVariables.java
+++ java-firsts-steps/src/main/TestVariables.java
@@ -3,12 +3,7 @@ package main;
 public class TestVariables {
 	 public static void main(String [] args){
 
-        System.out.println("Hola nuevo test");    
-
-        int edad = 37;
-
-        System.out.println(edad);
-        
-        System.out.println("La edad es "+ edad + ", Felicitaciones!");
+		 double division = 5.0 / 2;
+		 System.out.println (division);
     }
 }
