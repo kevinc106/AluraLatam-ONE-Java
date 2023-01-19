8d8c833d2131f58309d172f04b7a50555a818f0a
kevin camacho <kevin97camacho@gmail.com>
2023-01-19 10:59:09 -0400

    Test conditionals

---
+++ java-firsts-steps/src/main/TestConditional.java
@@ -0,0 +1,21 @@
+package main;
+
+public class TestConditional {
+	public static void main(String[] args) {
+		System.out.println("probando condicionales");
+	
+	    int edad = 20;
+	    int cantidadPersonas = 3;
+	
+	    if (edad >= 18) {
+	        System.out.println("Tienes más de 18 años");
+	        System.out.println("Bienvenido ");
+	    } else {
+	        if (cantidadPersonas >= 2) {
+	            System.out.println("No tienes 18 años, pero puedes ingresar, porque estás acompañado");
+	        } else {
+	            System.out.println("Lamentablemente no puedes ingresar");
+	        }
+	    }
+	}
+}
