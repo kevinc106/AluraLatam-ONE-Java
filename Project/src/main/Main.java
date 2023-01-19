fa40343a36473fd50278cb95e7023b001e79b55b
kevin camacho <kevin97camacho@gmail.com>
2023-01-19 13:54:37 -0400

    Composition

--- Project/src/main/Main.java
+++ Project/src/main/Main.java
@@ -4,15 +4,15 @@ public class Main {
 	public static void main(String[] args) {
 		Account firstAccount = new Account();
         firstAccount.setBalance(200);
-        firstAccount.deposit(100);
-        System.out.println(firstAccount.getBalance());
-        boolean result = firstAccount.withdraw(50);
-        System.out.println(firstAccount.getBalance());
+        firstAccount.deposit(100); 
+        boolean result = firstAccount.withdraw(50); 
         
         Account secondAccount = new Account();
         secondAccount.setBalance(100);
-        firstAccount.transfer(100, secondAccount);
-        System.out.println(firstAccount.getBalance());
-        System.out.println(secondAccount.getBalance());
+        firstAccount.transfer(100, secondAccount); 
+        
+        firstAccount.setClient(new Client());
+        firstAccount.getClient().name="oscar";
+        System.out.println(firstAccount.getClient().name);
 	}
 }
