f3d7086ecedebdad663ea6eccc64763f3d400b03
kevin camacho <kevin97camacho@gmail.com>
2023-01-19 11:08:13 -0400

    Test scope

---
+++ java-firsts-steps/src/main/TestScope.java
@@ -0,0 +1,25 @@
+package main;
+
+public class TestScope {
+
+	public static void main(String[] args) {
+        System.out.println ("pruebas condicionales");
+
+        int edad = 20;
+        int cantidadPersonas = 3;
+        boolean acompañado;
+
+        if (cantidadPersonas >= 2) {
+            acompañado = true;
+        } else {
+            acompañado = false;
+        }
+
+        if (edad >= 18 || acompañado) {
+            System.out.println ("Bienvenido");
+        } else {
+            System.out.println ("desafortunadamente no puedes ingresar");
+        }
+	}
+
+}
