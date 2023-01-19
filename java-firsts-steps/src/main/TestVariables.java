65c25a58c4a4d7c76d942fa5bc9816a879819b48
kevin camacho <kevin97camacho@gmail.com>
2023-01-19 09:17:41 -0400

    First commit

---
+++ java-firsts-steps/src/main/TestVariables.java
@@ -0,0 +1,14 @@
+package main;
+
+public class TestVariables {
+	 public static void main(String [] args){
+
+        System.out.println("Hola nuevo test");    
+
+        int edad = 37;
+
+        System.out.println(edad);
+        
+        System.out.println("La edad es "+ edad + ", Felicitaciones!");
+    }
+}
