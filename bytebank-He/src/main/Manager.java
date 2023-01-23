14745b23c0a7230d43a41c65a6cf59f3a0bc192d
kevin camacho <kevin97camacho@gmail.com>
2023-01-23 10:34:05 -0400

    Add inheritance

--- bytebank-He/src/main/Manager.java
+++ bytebank-He/src/main/Manager.java
@@ -1,29 +1,8 @@
 package main;
 
-public class Manager {
-	private String name;
-	private String document;
-	private double Salary;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getDocument() {
-		return document;
-	}
-	public void setDocument(String document) {
-		this.document = document;
-	}
-	public double getSalary() {
-		return Salary;
-	}
-	public void setSalary(double salary) {
-		Salary = salary;
-	}
-	
+public class Manager extends Officer { 
+	 
 	public double getBonification() {
-		return this.Salary;
+		return this.getSalary();
 	}
 }
--- bytebank-He/src/main/TestOfficer.java
+++ bytebank-He/src/main/TestOfficer.java
@@ -3,11 +3,16 @@ package main;
 public class TestOfficer {
 	public static void main(String[] args) {
 		Officer diego = new Officer();
+		Manager juan = new Manager();
 		diego.setName("Diego");
 		diego.setDocument("123");
 		diego.setSalary(1200);
+		juan.setName("Juan");
+		juan.setDocument("456");
+		juan.setSalary(4000);
 		System.out.println(diego.getSalary());
 		System.out.println(diego.getBonification());
+		System.out.println(juan.getBonification());
 		
 	}
 }
