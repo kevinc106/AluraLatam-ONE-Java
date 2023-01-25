687abb80e81cdde718c949bea95f76c0bddc2376
kevin camacho <kevin97camacho@gmail.com>
2023-01-25 01:08:17 -0400

    Exceptions with resources

---
+++ ExceptionsProject/src/TestConexion.java
@@ -0,0 +1,23 @@
+
+public class TestConexion {
+	public static void main(String[] args) {
+		
+		//Use with resources that need to be close 
+		try (Conexion con = new Conexion()) {
+			con.leerDatos();
+		} catch (Exception e) {
+			System.out.println("recibiendo exception");
+			e.printStackTrace();
+		}
+		//Use this with objects
+		/*Conexion con = new Conexion();
+		try {			
+			con.leerDatos();
+		} catch (IllegalStateException e) {
+			System.out.println("recibiendo exception");
+			e.printStackTrace();
+		}finally {			
+			con.cerrar();
+		}*/
+	}
+}
